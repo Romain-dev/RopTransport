750c571a7dc34e52631b0aaba5caa22bcda3c948
Romain <galli.romain24@gmail.com>
2015-06-01 18:24:30 +0200

    nd

--- src/Vogel.java
+++ src/Vogel.java
@@ -14,11 +14,20 @@ public class Vogel {
 		calculMatriceInverseDesCouts();
 		
 		while(isRessourcesAllouables(matriceCalculs)) {
+			nettoyerMatriceCouts();
+			calculMatriceInverseDesCouts();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
 			affecterMatriceRegrets();
+			afficheMatriceCouts();
 			afficherMatriceRegrets();
 			
-			/*int[] coordonnes = getCoordonnes(cloneMatriceCouts);
-			
+			int[] coordonnes = getCoordonnes();
+			System.out.println();
+			System.out.println("coordonnées:"+ coordonnes[0] + " "+ coordonnes[1]);
+			System.out.println();
 			
 			if(matriceCalculs[coordonnes[0]][matriceCalculs[0].length-1] >= matriceCalculs[matriceCalculs.length-1][coordonnes[1]])
 			{
@@ -34,7 +43,7 @@ public class Vogel {
 				
 			}
 			
-			Main.afficheMatriceCalculs();*/
+			Main.afficheMatriceCalculs();
 		}
 	}
 	
@@ -66,7 +75,7 @@ public class Vogel {
 		{
 			float[] element = copierLigneEtEnleverZero(matriceCouts[i]);
 			Arrays.sort(element);
-			if(element.length != 0)
+			if(element.length > 1)
 			{
 				matriceDesRegrets[i][matriceDesRegrets[i].length-1] = element[1] - element[0];
 			}
@@ -76,7 +85,7 @@ public class Vogel {
 		{
 			float[] element = copierLigneEtEnleverZero(matriceInverseDesCouts[i]);
 			Arrays.sort(element);
-			if(element.length != 0)
+			if(element.length > 1)
 			{
 				matriceDesRegrets[matriceDesRegrets.length-1][i] = element[1] - element[0]; 	
 			}
@@ -94,26 +103,136 @@ public class Vogel {
 		}
 		
 		float[] element = new float[total];
+		int index = 0;
 		
-		for(int i = 0; i < total; i++)
+		for(int i = 0; i < ligne.length; i++)
 		{
-			element[i] = ligne[i];
+			if(ligne[i] != 0)
+			{
+				element[index] = ligne[i];
+				index++;
+			}
 		}
 		
 		return element;
 	}
 	
-		public static void afficherMatriceRegrets() {
+	public static void afficherMatriceRegrets() {
+		System.out.println();
+		System.out.println();
+		System.out.println("Matrice des regrets");
+		System.out.println();
+		for(int i = 0; i < matriceDesRegrets.length; i++)
+		{
+			for(int j = 0; j < matriceDesRegrets[i].length; j++)
+				System.out.print(" " + matriceDesRegrets[i][j] + " |");
+				
 			System.out.println();
-			System.out.println();
-			System.out.println("Matrice des regrets");
-			System.out.println();
-			for(int i = 0; i < matriceDesRegrets.length; i++)
+		}
+	}
+	
+	public static int[] getCoordonnes() {
+		int[] result = new int[2];
+		
+		int index = -1;
+		boolean isVertical = true;
+		float valeur = -1;
+		
+		for(int i = 0; i < matriceDesRegrets.length; i++)
+		{
+			if(matriceDesRegrets[i][matriceDesRegrets[0].length-1] > valeur)
 			{
-				for(int j = 0; j < matriceDesRegrets[i].length; j++)
-					System.out.print(" " + matriceDesRegrets[i][j] + " |");
-					
-				System.out.println();
+				valeur = matriceDesRegrets[i][matriceDesRegrets[0].length-1];
+				index = i;
 			}
 		}
+		
+		for(int i = 0; i < matriceDesRegrets[0].length; i++)
+		{
+			if(matriceDesRegrets[matriceDesRegrets.length-1][i] > valeur)
+			{
+				valeur = matriceDesRegrets[i][matriceDesRegrets[0].length-1];
+				index = i;
+				isVertical = false;
+			}
+		}
+		
+		if(isVertical) 
+		{
+			int idexx = -1;
+			float val = 1000000;
+			
+			for(int i = 0; i < matriceCouts[0].length;i++)
+			{
+				if(matriceCouts[index][i] < val && matriceCouts[index][i] != 0)
+				{
+					idexx = i;
+					val = matriceCouts[index][i];
+				}
+			}
+			
+			result[0] = index;
+			result[1] = idexx;
+		}
+		else
+		{
+			int idexx = -1;
+			float val = 1000000;
+			
+			for(int i = 0; i < matriceCouts.length;i++)
+			{
+				if(matriceCouts[i][index] < val && matriceCouts[i][index] != 0)
+				{
+					idexx = i;
+					val = matriceCouts[index][i];
+				}
+			}
+			
+			result[0] = idexx;
+			result[1] = index;
+		}
+		
+		
+		return result;
+	}
+
+	//Affiche la matrice des couts dans la console.
+	public static void afficheMatriceCouts() {
+		System.out.println();
+		System.out.println();
+		System.out.println("Matrice des couts");
+		System.out.println(); 
+		
+		for(int i = 0; i < matriceCouts.length; i++)
+		{
+			for(int j = 0; j < matriceCouts[i].length; j++)
+				System.out.print(" " + matriceCouts[i][j] + " |");
+				
+			System.out.println();
+		}
+	}
+	
+	public static void nettoyerMatriceCouts() {
+		for(int i = 0; i < matriceCouts.length;i++)
+		{
+			if(matriceCalculs[i][matriceCalculs[0].length-1] == 0)
+			{
+				for(int j = 0; j<matriceCouts[0].length;j++)
+				{
+					matriceCouts[i][j] = 0;
+				}
+			}
+		}
+		
+		for(int i = 0; i < matriceCouts[0].length;i++)
+		{
+			if(matriceCalculs[matriceCalculs.length-1][i] == 0)
+			{
+				for(int j = 0; j<matriceCouts.length;j++)
+				{
+					matriceCouts[j][i] = 0;
+				}
+			}
+		}
+	}
 }    	
